90a821b33bc71613f046a07bc368cbbb1a8c1694
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-09 14:19:27 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,50 +15,51 @@ f1_odds = []
 f2_odds = []
 header = []
 table_div = []
+top = []
+
 
 def scrape_data():
     # set up page to extract table
     data = requests.get("https://www.oddschecker.com/ufc-mma")
     soup = BeautifulSoup(data.text, 'html.parser')
-    
-    h1 = soup.find ('h1')
+
+    h1 = soup.find('h1')
     header.append(h1.text)
-    
-    table_div = soup.find ('div' , {'class' : 'content-4'})
 
-    table = table_div.find('table' , {'class': 'all-odds-click'})
+    table_div = soup.find('div', {'class': 'content-4'})
 
-    header =table.find_all('tr' , {'class': 'hda-header'})
+    table = table_div.find('table', {'class': 'all-odds-click'})
+
+    top = table.find_all('tr', {'class': 'hda-header'})
 
     fighter = table.find_all('td', {'class': 'all-odds-click'})
 
-    c=fighter[0].text.strip()
-    d=fighter[1].text.strip()
+    c = fighter[0].text.strip()
+    d = fighter[1].text.strip()
 
     f1.append(c)
     f2.append(d)
 
-
     odds = table.find_all('td', {'class': 'basket-add'})
 
-
-    a=odds[0].text.strip()
-    b=odds[1].text.strip()
+    a = odds[0].text.strip()
+    b = odds[1].text.strip()
 
     f1_odds.append(a)
     f2_odds.append(b)
 
-
     return None
+
 
 def create_df():
     df = pd.DataFrame()
-    df["Fighter1"]= f1
+    df["Fighter1"] = f1
     df["Fighter1_Odds"] = f1_odds
-    df["Fighter2"]= f2
-    df["Fighter2_Odds"]= f2_odds
+    df["Fighter2"] = f2
+    df["Fighter2_Odds"] = f2_odds
 
     return df
+
 
 scrape_data()
 df = create_df()
@@ -67,3 +68,4 @@ conn = sqlite3.connect('data.sqlite')
 df.to_sql('data', conn, if_exists='replace')
 print('Db successfully constructed and saved')
 conn.close()
+

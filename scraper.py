c0a8ea40328913b5fd11a9afa9d1d265975643fe
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-12 17:08:08 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,7 +9,6 @@ import sqlite3
 import time
 import os
 
-
 links = []
 alphabets = sorted(set(string.ascii_lowercase))
 f1 = []
@@ -20,64 +19,59 @@ f2_odds = []
 
 def scrape_data():
     for alpha in alphabets:
-        links.append(https://sports.williamhill.com/betting/en-gb/ufc"")
+        links.append("https://sports.williamhill.com/betting/en-gb/ufc")
 
         # now that we have a list of links we need to iterate it with BeautifulSoup
-    for link in links:
-        print(f"Currently on this link: {link}")
+        for link in links:
+            print(f"Currently on this link: {link}")
 
-        data = requests.get(link)
-        soup = BeautifulSoup(data.text, 'html.parser')
-        names = soup.find_all('a', {'class': 'btmarket__name btmarket__name--featured'}, href=True) 
-        
-       
-    # list to store url page of fighters
-        fighters = []
-
-        for name in names:
-            fighters.append(name['href'])
-
-        fighters = sorted(set(fighters))
-
-        for fighter in fighters:
-            data = requests.get(fighter)
+            data = requests.get(link)
             soup = BeautifulSoup(data.text, 'html.parser')
-            time.sleep(2)
+            names = soup.find_all('a', {'class': 'btmarket__name btmarket__name--featured'}, href=True)
 
-        
-        fighters = soup.find('p', {'class':"btmarket__name"})
-        c = fighters[0].text.strip()
-        d = fighters[1].text.strip()
+            # list to store url page of fighters
+            fighters = []
 
-        f1.append(c)
-        f2.append(d)
+            for name in names:
+                fighters.append(name['href'])
 
-        odds = fight.find('button', {'class': "btn betbutton oddsbutton"})
+            fighters = sorted(set(fighters))
 
-        a = odds[0].text.strip()
-        b = odds[1].text.strip()
+            for fighter in fighters:
+                data = requests.get(fighter)
+                soup = BeautifulSoup(data.text, 'html.parser')
+                time.sleep(2)
 
-        f1_odds.append(a)
-        f2_odds.append(b)
+            fighters = soup.find('p', {'class': "btmarket__name"})
+            c = fighters[0].text.strip()
+            d = fighters[1].text.strip()
 
-    return None
+            f1.append(c)
+            f2.append(d)
 
+            odds = soup.find('button', {'class': "btn betbutton oddsbutton"})
 
-def create_df():
-    df = pd.DataFrame()
-    df["Fighter1"] = f1
-    df["Fighter1_Odds"] = f1_odds
-    df["Fighter2"] = f2
-    df["Fighter2_Odds"] = f2_odds
+            a = odds[0].text.strip()
+            b = odds[1].text.strip()
 
-    return df
+            f1_odds.append(a)
+            f2_odds.append(b)
 
+        return None
 
-scrape_data()
-df = create_df()
+    def create_df():
+        df = pd.DataFrame()
+        df["Fighter1"] = f1
+        df["Fighter1_Odds"] = f1_odds
+        df["Fighter2"] = f2
+        df["Fighter2_Odds"] = f2_odds
 
-conn = sqlite3.connect('data.sqlite')
-df.to_sql('data', conn, if_exists='replace')
-print('Db successfully constructed and saved')
-conn.close()
+        return df
 
+    scrape_data()
+    df = create_df()
+
+    conn = sqlite3.connect('data.sqlite')
+    df.to_sql('data', conn, if_exists='replace')
+    print('Db successfully constructed and saved')
+    conn.close()

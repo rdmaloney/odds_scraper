b66a5249776e83b2c4d57c6efe384f0a03a72c27
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-13 21:32:04 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,11 +11,13 @@ f2 = []
 f1_odds = []
 f2_odds = []
 
+
 def safe_eval(expr):
     try:
-        return round(eval(expr),2)
+        return round(eval(expr), 2)
     except:
         return expr
+
 
 def scrape_data():
     data = requests.get("https://sports.williamhill.com/betting/en-gb/ufc")
@@ -29,24 +31,25 @@ def scrape_data():
         buttons_having_odds = div.findAll('button')
         f1_odds = safe_eval(buttons_having_odds[0]["data-odds"])
         f2_odds = safe_eval(buttons_having_odds[1]["data-odds"])
-        
 
 
 scrape_data()
 
-    def create_df():
-        df = pd.DataFrame()
-        df["Fighter1"] = f1
-        df["Fighter1_Odds"] = f1_odds
-        df["Fighter2"] = f2
-        df["Fighter2_Odds"] = f2_odds
 
-        return df
+def create_df():
+    df = pd.DataFrame()
+    df["Fighter1"] = f1
+    df["Fighter1_Odds"] = f1_odds
+    df["Fighter2"] = f2
+    df["Fighter2_Odds"] = f2_odds
 
-    scrape_data()
-    df = create_df()
+    return df
 
-    conn = sqlite3.connect('data.sqlite')
-    df.to_sql('data', conn, if_exists='replace')
-    print('Db successfully constructed and saved')
-    conn.close()
+
+scrape_data()
+df = create_df()
+
+conn = sqlite3.connect('data.sqlite')
+df.to_sql('data', conn, if_exists='replace')
+print('Db successfully constructed and saved')
+conn.close()

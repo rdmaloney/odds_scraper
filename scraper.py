eb96771725550e65c50c5039d46f33174339fcd7
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-29 23:29:35 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -44,8 +44,8 @@ def scrape_data():
         p1 = names[0].text
         p2 = names[1].text
         buttons_having_odds = div.findAll('button')
-        p1_odds = Fraction(buttons_having_odds[0]["data-odds"])
-        p2_odds = Fraction(buttons_having_odds[1]["data-odds"])
+        p1_odds = frac(Fraction(buttons_having_odds[0]["data-odds"]))
+        p2_odds = frac(Fraction(buttons_having_odds[1]["data-odds"]))
         f1.append(p1)
         f2.append(p2)
         f1_odds.append(p1_odds)
